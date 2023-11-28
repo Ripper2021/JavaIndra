d48a263bb14911d0cc0081abac7e135194aa9287
Ripper2021 <95532358+Ripper2021@users.noreply.github.com>
2023-11-28 08:01:53 -0300

    Add files via upload

---
+++ Clase11/src/ClaseSimple.java
@@ -0,0 +1,10 @@
+
+public class ClaseSimple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Agrego una linea mas");	
+		System.out.println("Hola Mundo!"); 
+	}
+
+}
